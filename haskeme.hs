0400dbc79c354310c6dedd3b12f77f2eb07b14f4
Nicholas Quirk <nmquirk@gmail.com>
2013-06-23 19:58:38 -0400

    Added some stuff.

--- haskeme.hs
+++ haskeme.hs
@@ -16,7 +16,7 @@ main = do {
 }
 
 symbol :: Parser Char
-symbol = oneOf "!$%&|*+-/:<=>?@^_~#"
+symbol = oneOf "!$%&|*+-/:<=>?@^_~"
 
 readOrThrow :: Parser a -> String -> ThrowsError a
 readOrThrow parser input = case parse parser "lisp" input of
@@ -35,16 +35,47 @@ data LispVal = Atom String
              | Number Integer
              | String String
              | Bool Bool
+             | Character Char
              | Port Handle
              | PrimitiveFunc ([LispVal] -> ThrowsError LispVal)
              | IOFunc ([LispVal] -> IOThrowsError LispVal)
              | Func {params :: [String], vararg :: (Maybe String), body :: [LispVal], closure :: Env}
 
+escapeChars :: Parser String
+escapeChars = do {
+    char '\\';
+    x <- oneOf "\\\"ntr";
+    case x of {
+        '\\' -> do { return [x] };
+        '"' -> do { return [x] };
+        't' -> do { return "\t" };
+        'n' -> do { return "\n" };
+        'r' -> do { return "\r" };
+    }
+}
+
+parseChar :: Parser LispVal
+parseChar = do {
+    try $ string "#\\";
+    x <- parseCharName <|> anyChar;
+    return $ Character x;
+}
+
+parseCharName = do {
+    x <- try (string "space" <|> string "newline");
+    case x of {
+        "space" -> do { return ' ' };
+        "newline" -> do { return '\n' };
+    }
+}
+
 parseString :: Parser LispVal
-parseString = do char '"'
-                 x <- many (noneOf "\"")
-                 char '"'
-                 return $ String x
+parseString = do {
+    char '"';
+    x <- many $ many1 (noneOf "\"\\") <|> escapeChars;
+    char '"';
+    return $ String (concat x);
+}
 
 parseAtom :: Parser LispVal
 parseAtom = do first <- letter <|> symbol
@@ -56,7 +87,51 @@ parseAtom = do first <- letter <|> symbol
                           otherwise -> Atom atom
 
 parseNumber :: Parser LispVal
-parseNumber = liftM (Number . read) $ many1 digit
+parseNumber = many1 digit >>= return . Number . read
+--parseNumber = do {
+--    num <- parseDigital1 <|> parseDigital2 <|> parseHex <|> parseOct <|> parseBin;
+--    return $ num;
+--}
+
+--parseDigital1 :: Parser LispVal;
+--parseDigital1 = do {
+--    x <- many1 digit;
+--    (return . Number . read) x;
+--}
+
+--parseDigital2 :: Parser LispVal
+--parseDigital2 = do {
+--    try $ string "#d";
+--    x <- many1 digit;
+--    (return . Number . read) x;
+--}
+
+--parseHex :: Parser LispVal
+--parseHex = do {
+--    try $ string "#x";
+--    x <- many1 hexDigit;
+--    return $ Number (hex2dig x);
+--}
+
+--parseOct :: Parser LispVal
+--parseOct = do {
+--    try $ string "#o";
+--    x <- many1 octDigit;
+--    return $ Number (oct2dig x);
+--}
+
+--parseBin :: Parser LispVal
+--parseBin = do {
+--    try $ string "#b";
+--    x <- many1 (oneOf "10");
+--    return $ Number (bin2dig x);
+--}
+
+--oct2dig x = fst $ readOct x !! 0
+--hex2dig x = fst $ readHex x !! 0
+--bin2dig = bin2dig' 0
+--bin2dig' digint "" = digint
+--bin2dig' digint (x : xs) = let old = 2 * digint + (if x == '0' then 0 else 1) in bin2dig' old xs
 
 parseList :: Parser LispVal
 parseList = liftM List $ sepBy parseExpr spaces
@@ -73,10 +148,22 @@ parseQuoted = do
     x <- parseExpr
     return $ List [Atom "quote", x]
 
+parseBool :: Parser LispVal
+parseBool = do {
+    string "#";
+    x <- oneOf "tf";
+    return $ case x of {
+        't' -> Bool True;
+        'f' -> Bool False;
+    }
+}
+
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
         <|> parseString
-        <|> parseNumber
+        <|> try parseNumber
+        <|> try parseBool
+        <|> try parseChar
         <|> parseQuoted
         <|> do char '('
                x <- (try parseList) <|> parseDottedList
